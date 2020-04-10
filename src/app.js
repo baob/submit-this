2344ffe6a7f81e344c3a97cd1842650faae4a9d3
baob <coder@onesandthrees.com>
2020-04-10 17:29:44 +0100

    WIP: adding web client

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const { createEventAdapter } = require('@slack/events-api');
+const { WebClient } = require('@slack/web-api');
 const handleAppMention = require('./handleAppMention');
 const dotenv = require('dotenv')
 
 const app = () => {
 
     dotenv.config(); // TODO: later, we'll need to bypass this for NODE_ENV production
+
+    /*
+        Set up the Event Listener
+    */
 
     const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
     const port = process.env.PORT || 3000;
@@ -15,6 +20,17 @@ const app = () => {
     slackEvents.on('error', (error) => {
         console.log(error.name); // TypeError
     });
+
+    /*
+        Set up the WebClient
+    */
+
+    // Read a token from the environment variables
+    const token = process.env.SLACK_TOKEN;
+
+    // Initialize
+    const web = new WebClient(token);
+
 
     // Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
     slackEvents.on('app_mention', handleAppMention);
