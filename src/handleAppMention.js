06a7ce4041fa20e7aa0c4a2cd3174759942e3ec6
baob <coder@onesandthrees.com>
2020-04-10 17:55:54 +0100

    handler juse echoes back the message

--- src/handleAppMention.js
+++ src/handleAppMention.js
@@ -1,8 +1,17 @@
 
-const handleAppMention = (event, _web) => {
+const handleAppMention = async (event, web) => {
     console.log(`Received a app_mention event: user ${event.user} in channel ${event.channel} says ${event.text}`);
     console.log('---- event:', event);
-    // console.log('---- web:', _web);
+    // console.log('---- web:', web);
+
+
+    const result = await web.chat.postMessage({
+        text: `You said: ${event.text}`,
+        channel: event.channel,
+    });
+
+    console.log('result of postMessage', result)
+
 };
 
 module.exports = handleAppMention
