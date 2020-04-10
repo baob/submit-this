4b1795723fe7bfc610dbd569464b6a7c133ef861
baob <coder@onesandthrees.com>
2020-04-10 20:50:17 +0100

    okay, seems we cant rely on the blocks/elements being there

--- src/extractData.js
+++ src/extractData.js
@@ -1,13 +1,5 @@
 
 const extractData = (event) => {
-    console.log('event.blocks', event.blocks);
-    event.blocks.forEach((block) => {
-        console.log('block', block);
-        block.elements.forEach((element) => {
-            console.log('element', element);
-        });
-    });
-
     return {
         message: event.text
     }
