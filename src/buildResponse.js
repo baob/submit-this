629503ba9778e6b60ff6135865c34699ce99aae9
baob <coder@onesandthrees.com>
2020-04-10 21:13:50 +0100

    encode the data for uri

--- src/buildResponse.js
+++ src/buildResponse.js
@@ -3,8 +3,9 @@ const buildResponse = (template, data) => {
     let result = template
 
     Object.keys(data).forEach((key) => {
-        token = `{{${key}}}`
-        result = result.replace(token, data[key])
+        const token = `{{${key}}}`
+        const encodedText = encodeURIComponent(data[key])
+        result = result.replace(token, encodedText)
     });
 
     return result
