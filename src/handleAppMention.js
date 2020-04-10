e39e2296ee95fc7fae88d5eda77f6881a2ce506e
baob <coder@onesandthrees.com>
2020-04-10 16:48:28 +0100

    add debugging

--- src/handleAppMention.js
+++ src/handleAppMention.js
@@ -1,6 +1,7 @@
 
 const handleAppMention = (event) => {
     console.log(`Received a app_mention event: user ${event.user} in channel ${event.channel} says ${event.text}`);
+    console.log('---- event:', event);
 };
 
 module.exports = handleAppMention
