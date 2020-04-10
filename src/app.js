ef6346176ae29087e6a8bb9dc22fc37ada50cc43
baob <coder@onesandthrees.com>
2020-04-10 16:43:00 +0100

    add cribbed error handler

--- src/app.js
+++ src/app.js
@@ -11,13 +11,18 @@ const app = () => {
 
     const slackEvents = createEventAdapter(slackSigningSecret);
 
+    // All errors in listeners are caught here. If this weren't caught, the program would terminate.
+    slackEvents.on('error', (error) => {
+        console.log(error.name); // TypeError
+    });
+
     // Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
     slackEvents.on('app_mention', handleAppMention);
 
     (async () => {
         const server = await slackEvents.start(port);
         console.log(`Listening for events on ${server.address().port}`);
-        console.log('---- sever', server)
+        // console.log('---- server', server)
     })();
 
 }
