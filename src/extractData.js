6b5dc56fbbe1b7179b4c1f0597f08a37c5807897
baob <coder@onesandthrees.com>
2020-04-10 21:04:10 +0100

    strip the bot name out of the message

--- src/extractData.js
+++ src/extractData.js
@@ -1,7 +1,13 @@
 
 const extractData = (event) => {
+    const userMatch = event.text.trim().match(/^(<@U.{10,12}>)/i)
+    const text = (userMatch ? event.text.replace(userMatch[0], '') : event.text).trim();
+
+    // console.log('userMatch:', userMatch)
+    // console.log('text:', text)
+
     return {
-        message: event.text
+        message: text
     }
 };
 
