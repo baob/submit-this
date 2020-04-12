74ddf67856028061a71a508dc8f8050755ca7643
baob <coder@onesandthrees.com>
2020-04-12 08:14:49 +0100

    refactoring to add help

--- src/handleAppMention.js
+++ src/handleAppMention.js
@@ -1,29 +1,19 @@
 const extractData = require('./extractData');
-const buildResponse = require('./buildResponse')
-const ENCODE_THE_DATA = true
+const processSubmission = require('./processSubmission');
 
 const handleAppMention = async (event, web) => {
     console.log(`Received a app_mention event: user ${event.user} in channel ${event.channel} says ${event.text}`);
     // console.log('---- event:', event);
     // console.log('---- web:', web);
 
-    const data = extractData(event)
+    const { message } = extractData(event)
 
-    const messageTemplate = process.env.SUBMIT_MESSAGE_TEMPLATE
-    const linkTemplate = process.env.SUBMIT_LINK_TEMPLATE
+    if (message == '' || message == 'help') {
+        // sendHelp(event, web);
+        return
+    }
 
-    const link = buildResponse(linkTemplate, data, ENCODE_THE_DATA)
-    const response = buildResponse(messageTemplate, { link, ...data })
-
-    console.log('---- response:', response);
-    try {
-        await web.chat.postMessage({
-            text: `<@${event.user}> ${response}`,
-            channel: event.channel
-        });
-    } catch (err) {
-        console.log('slack web api rejected the chat.postMessage request with', err)
-    };
+    processSubmission(event, web);
 };
 
 module.exports = handleAppMention
---
+++ src/processSubmission.js
@@ -0,0 +1,29 @@
+const extractData = require('./extractData');
+const buildResponse = require('./buildResponse')
+const ENCODE_THE_DATA = true
+
+const processSubmission = async (event, web) => {
+    console.log(`Processing submission: user ${event.user} in channel ${event.channel} says ${event.text}`);
+    // console.log('---- event:', event);
+    // console.log('---- web:', web);
+
+    const data = extractData(event)
+
+    const messageTemplate = process.env.SUBMIT_MESSAGE_TEMPLATE
+    const linkTemplate = process.env.SUBMIT_LINK_TEMPLATE
+
+    const link = buildResponse(linkTemplate, data, ENCODE_THE_DATA)
+    const response = buildResponse(messageTemplate, { link, ...data })
+
+    console.log('---- response:', response);
+    try {
+        await web.chat.postMessage({
+            text: `<@${event.user}> ${response}`,
+            channel: event.channel
+        });
+    } catch (err) {
+        console.log('slack web api rejected the chat.postMessage request with', err)
+    };
+};
+
+module.exports = processSubmission
