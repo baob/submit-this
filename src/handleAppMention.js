dff70be5d1eac1d921aecda4300774964bf976c4
baob <coder@onesandthrees.com>
2020-04-12 09:11:16 +0100

    add introduction

--- src/handleAppMention.js
+++ src/handleAppMention.js
@@ -1,6 +1,7 @@
 const extractData = require('./extractData');
 const processSubmission = require('./processSubmission');
 const sendHelp = require('./sendHelp');
+const sendIntroduction = require('./sendIntroduction')
 
 const handleAppMention = async (event, web) => {
     console.log(`Received a app_mention event: user ${event.user} in channel ${event.channel} says ${event.text}`);
@@ -11,6 +12,10 @@ const handleAppMention = async (event, web) => {
 
     if (message == '' || message == 'help') {
         sendHelp(event, web);
+        return
+    }
+    if (message == '?' || message.includes('who are you')) {
+        sendIntroduction(event, web);
         return
     }
 
---
+++ src/sendIntroduction.js
@@ -0,0 +1,26 @@
+const packageJson = require('../package.json');
+
+const sendIntroduction = async (event, web) => {
+    console.log(`Sending introduction: user ${event.user} in channel ${event.channel} says ${event.text}`);
+    // console.log('---- event:', event);
+    // console.log('---- web:', web);
+    const { name, version } = packageJson
+
+    const response = [
+        `Hi. I am a bot running the ${name} software,  version ${version}.`,
+        'I am here to help with submitting your data',
+        'Just send me a message with the text you want to submit, and I will get you started.'
+    ].join('\n');
+
+    console.log('---- response:', response);
+    try {
+        await web.chat.postMessage({
+            text: `<@${event.user}> ${response} `,
+            channel: event.channel
+        });
+    } catch (err) {
+        console.log('slack web api rejected the chat.postMessage request with', err)
+    };
+};
+
+module.exports = sendIntroduction
