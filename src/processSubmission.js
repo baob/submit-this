997d4d507d59fd6863323d89f052c2d3fcce2d40
baob <coder@onesandthrees.com>
2020-04-13 13:02:49 +0100

    opengraph: first cut of the functionality
    This is a combination of 5 commits.
    
    WIP: the callback version
    WIP: promisy version with no catch as yet
    WIP: inline code for open graph
    link ambiguity fixed
    deal with errors, except, it doesnt seem to work with promises

--- src/processSubmission.js
+++ src/processSubmission.js
@@ -1,5 +1,8 @@
 const extractData = require('./extractData');
 const buildResponse = require('./buildResponse');
+const { promisify } = require('util');
+const openGraphScraper = promisify(require('open-graph-scraper'));
+
 const ENCODE_THE_DATA = true;
 
 const processSubmission = async (event, web) => {
@@ -9,14 +12,42 @@ const processSubmission = async (event, web) => {
     // console.log('---- event:', event);
     // console.log('---- web:', web);
 
-    const data = extractData(event);
+    let data = extractData(event);
     console.log('---- extracted data from message:', data);
+
+    let scraperError = false;
+    let scraperResponse;
+    try {
+        scraperResponse = await openGraphScraper({ url: data.link });
+        const {
+            ogDescription,
+            ogImage,
+            ogSiteName,
+            ogTitle,
+            ogUrl,
+        } = scraperResponse.data;
+        const { url: ogImageUrl } = ogImage;
+        data = {
+            ogImageUrl,
+            ogDescription,
+            ogSiteName,
+            ogTitle,
+            ogUrl,
+            ...data,
+        };
+    } catch (error) {
+        console.log('openGraphScraper returned error:', error);
+        scraperError = true;
+    }
+    if (scraperError) {
+        console.log('scraper error return:', scraperResponse);
+    };
 
     const messageTemplate = process.env.SUBMIT_MESSAGE_TEMPLATE;
     const linkTemplate = process.env.SUBMIT_LINK_TEMPLATE;
 
-    const link = buildResponse(linkTemplate, data, ENCODE_THE_DATA);
-    const response = buildResponse(messageTemplate, { link, ...data });
+    const submitLink = buildResponse(linkTemplate, data, ENCODE_THE_DATA);
+    const response = buildResponse(messageTemplate, { submitLink, ...data });
 
     console.log('---- response:', response);
     try {
